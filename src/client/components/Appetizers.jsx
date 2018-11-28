5570d2c2baa27ae61912e1fa63229d3efcef1e8e
hkim9497 <hari9497@gmail.com>
2018-11-28 10:16:19 -0800

    finalized skeleton for redux and connect to react components

--- src/client/components/Appetizers.jsx
+++ src/client/components/Appetizers.jsx
@@ -1,13 +1,36 @@
-import React, {Component} from 'React';
+import React, { Component } from 'React';
+import { connect } from 'react-redux';
+import * as actions from '../actions/orderActions';
+
+const mapStateToProps = store => {
+  return {
+    productList: store.product.products
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchProducts: (category) => {
+      dispatch(actions.getProducts(category));
+    }
+  }
+}
 
 class Appetizers extends Component {
+  constructor(props) {
+    super(props);
+  }
   componentDidMount() {
-    this.props.grabProductsFromDatabase('Appetizers');
+    this.props.fetchProducts('Appetizers');
   }
   render() {
+    const productName = this.props.productList.map((element) => {
+      return <li>{element.name}</li>
+    })
+
     return (
-      <div>Hello world</div>
+      <div>{productName}</div>
     )
   }
 }
-export default Appetizers;
+export default connect(mapStateToProps, mapDispatchToProps)(Appetizers);
--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,12 @@ const bodyParser = require('body-parser');
 const userController = require('./controllers/userController');
 const productController = require('./controllers/productController');
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:1234');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(bodyParser.json());
 // app.get('/', (req, res) => {
