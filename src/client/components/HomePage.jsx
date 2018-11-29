06413381665ec53940aa2e4bf288fd6891c05a30
g-es <gesun@usc.edu>
2018-11-29 10:30:57 -0800

    oauth

--- src/client/components/HomePage.jsx
+++ src/client/components/HomePage.jsx
@@ -1,13 +1,34 @@
 import React, {Component} from 'React';
 import {Link} from 'react-router-dom';
-const HomePage = (props) => {
-  return (
-    <div>
-      <button><Link to='/Appetizers'>Appetizers</Link></button>
-      <button><Link to='/Makimono'>Makimono</Link></button>
-      <button><Link to='/Yakitori'>Yakitori</Link></button>
-      <button><Link to='/Tempura'>Tempura</Link></button>
-    </div>
-  )
+class HomePage extends Component {
+  constructor(){
+    super();
+  }
+// componentDidMount(){
+  
+//     fetch('http://localhost:3000/auth/facebook', {
+//       headers: {'Content-Type': 'application/json'},
+//       mode:'no-cors'
+//     })
+//     .then((data) => {
+//       console.log('heee',data)
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+  
+// }
+  render(){
+
+    return (
+      <div>
+        <a href="http://localhost:3000/auth/facebook">whatttt</a>
+        <button><Link to='/Appetizers'>Appetizers</Link></button>
+        <button><Link to='/Makimono'>Makimono</Link></button>
+        <button><Link to='/Yakitori'>Yakitori</Link></button>
+        <button><Link to='/Tempura'>Tempura</Link></button>
+      </div>
+    )
+  }
 }
 export default HomePage;
--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,66 @@
 const express = require('express');
+const db = require('./postgresql.js');
 
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const userController = require('./controllers/userController');
 const productController = require('./controllers/productController');
+const passport = require('passport');
+const Strategy = require('passport-facebook').Strategy;
 
 
+passport.use(new Strategy({
+  clientID: '309065336363308',
+  clientSecret: '2d572aeed2b135ac9203ba185533d98a',
+  callbackURL: "http://localhost:3000/auth/facebook/callback",
+  profileFields: ['email','id', 'first_name', 'gender', 'last_name', 'picture']
+  
+},
+(accessToken, refreshToken, profile ,done) => {
+  
+  // console.log('got here',profile,'this is req',req)
+  //find user
+  // db.any("SELECT first_name, last_name FROM customer WHERE first_name= $1 AND last_name = $2", [profile.first_name, profile.last_name])
+  // .then((data) => {
+  //   if(data.length === 0) {
+  //     // create user?
+  //     // return res.status(300).send();
+  //     console.log('no user ')
+  //   } else{
+  //     // res.locals.data = data;
+  //     // return next();
+  //     return cb(null, profile);
+  //   }
+  // })
+  // .catch((err) => {
+  //   console.log('ERROR: ', err)
+  //   next(err);
+  // })
+  done(null, profile);
+}
+));
 
+// passport.serializeUser(function(user, done) {
+//   console.log('what happened')
+//   done(null, user);
+// });
+
+// passport.deserializeUser(function(user, done) {
+//   console.log('what happened2')
+//   done(null, user);
+// });
+
+app.use(passport.initialize());
+// app.use(passport.session());
 app.use(bodyParser.json());
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname + '../../../index.html'));
 // });
+
+app.get('/login', (req,res) => {
+  res.json('hiiiiii');
+})
 app.get('/appetizers', productController.getAppetizers, (req, res) => {
   res.json(res.locals.info);
 });
@@ -28,6 +77,26 @@ app.post('/orders', productController.postOrder, (req, res) => {
   console.log('do what');
 })
 app.use(express.static(__dirname + '/../../dist'));
+
+
+
+app.get('/auth/facebook',passport.authenticate('facebook'));
+
+ 
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { session: false}), (req,res)=>{
+
+    // Successful authentication, redirect home.
+    console.log('bbbbb')
+    console.log(req.user,'reqqqq');
+
+    return res.redirect('/');
+    // res.send('auth worked maybe')
+  },
+);
+
+
+  
 
 module.exports = app.listen(3000, () => {
   console.log('Listening on port 3000...');
