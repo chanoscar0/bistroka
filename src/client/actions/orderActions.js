78f0cf93616befbcde31a8060374cf8bdbe55fee
hkim9497 <hari9497@gmail.com>
2018-11-29 10:32:19 -0800

    Inserted OrderListItem and Order Summary component and passed props down

--- src/client/actions/orderActions.js
+++ src/client/actions/orderActions.js
@@ -1,30 +1,34 @@
 import * as types from '../constants/actionTypes';
 
-export const addQuantity = (index) => ({
+export const addQuantity = event => ({
   type: types.ADD_QUANTITY,
-  payload: index,
+  payload: event,
 });
+
 export const addToCart = object => ({
   type: types.ADD_TO_CART,
   payload: object,
 });
-export const removeQuantity = (index) => ({
+
+export const removeQuantity = event => ({
   type: types.REMOVE_QUANTITY,
-  payload: index,
+  payload: event,
 });
+
+export const resetProducts = () => ({
+  type: types.RESET_PRODUCTS
+});
+
 export const resetQuantity = (index) => ({
   type: types.RESET_QUANTITY,
   payload: index
-})
-export const addToCartAndResetQuantity = object => {
+ })
+ export const addToCartAndResetQuantity = object => {
   return dispatch => {
     dispatch(addToCart(object));
     dispatch(resetQuantity(object.index));
   }
-}
-export const resetProducts = () => ({
-  type: types.RESET_PRODUCTS
-});
+ }
 // Redux-Thunk Asynchronous
 
 export const getProducts = (category) => {
@@ -40,17 +44,44 @@ export const getProducts = (category) => {
           product.quantity = 0;
           product.index = index;
           index += 1;
-        })
+        });
         dispatch(getProductsSuccess(productArray));
       })
       .catch(error => dispatch(getProductsFailure(error)));
   }
 }
+
 export const getProductsFailure = error => ({
   type: types.GET_PRODUCTS_FAILURE,
   payload: error,
 });
+
 export const getProductsSuccess = data => ({
   type: types.GET_PRODUCTS_SUCCESS,
+  payload: data,
+});
+
+export const checkout = (object) => {
+  return function (dispatch){
+    return fetch('/order', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(object),
+    })
+    .then((data) => data.json())
+    .then((data) => dispatch(checkoutSuccess(data)))
+    .catch((error) => dispatch(checkoutFailure(error)))
+  }
+}
+
+export const checkoutFailure = error => ({
+  type: types.CHECKOUT_FAILURE,
+  payload: error,
+});
+
+export const checkoutSuccess = data => ({
+  type: types.CHECKOUT_SUCCESS,
   payload: data,
 });
--- src/client/components/Item.jsx
+++ src/client/components/Item.jsx
@@ -1,12 +1,13 @@
-import React, {Component} from 'React';
-import {connect} from 'react-redux';
+import React, { Component } from 'React';
+import { connect } from 'react-redux';
 import * as actions from '../actions/orderActions';
+
 const mapDispatchToProps = (dispatch) => {
   return {
-    addToCart: (obj) => {dispatch(actions.addToCart(obj))},
-    lowerQuantity: (index) => {dispatch(actions.removeQuantity(index))},
-    addQuantity: (index) => {dispatch(actions.addQuantity(index))},
-    addToCartAndResetQuantity: (obj) => {dispatch(actions.addToCartAndResetQuantity(obj))},
+    addToCart: (obj) => { dispatch(actions.addToCart(obj)) },
+    lowerQuantity: (index) => { dispatch(actions.removeQuantity(index)) },
+    addQuantity: (index) => { dispatch(actions.addQuantity(index)) },
+    addToCartAndResetQuantity: (obj) => { dispatch(actions.addToCartAndResetQuantity(obj)) },
   }
 }
 const Item = (props) => {
@@ -19,19 +20,19 @@ const Item = (props) => {
   }
   const rightPanel = {
     'float': 'right',
-    'marginTop':'23px',
-    'marginLeft':'5px',
+    'marginTop': '23px',
+    'marginLeft': '5px',
     'marginRight': '5px'
   }
-  const objCopy = Object.assign({},props.passObj)
+  const objCopy = Object.assign({}, props.passObj)
   return (
-    <div style = {divStyle}>
-      <h2 style = {{'display': 'inline-block', 'marginRight': '5px'}}>{props.name}</h2><span style = {{'display': 'inline-block'}}>{props.price}</span>
-      <button  onClick = {() => props.addQuantity(props.index)} style = {rightPanel}>+</button>
-      <p style = {rightPanel}>{props.quantity}</p>
-      <button onClick = {() => props.lowerQuantity(props.index)} style = {rightPanel}>-</button>
-      <button onClick = {() =>props.addToCartAndResetQuantity(objCopy)} style = {rightPanel}>Add to Cart</button>
+    <div style={divStyle}>
+      <h2 style={{ 'display': 'inline-block', 'marginRight': '5px' }}>{props.name}</h2><span style={{ 'display': 'inline-block' }}>{props.price}</span>
+      <button onClick={() => props.addQuantity(props.index)} style={rightPanel}>+</button>
+      <p style={rightPanel}>{props.quantity}</p>
+      <button onClick={() => props.lowerQuantity(props.index)} style={rightPanel}>-</button>
+      <button onClick={() => props.addToCartAndResetQuantity(objCopy)} style={rightPanel}>Add to Cart</button>
     </div>
   )
-} 
+}
 export default connect(null, mapDispatchToProps)(Item);
